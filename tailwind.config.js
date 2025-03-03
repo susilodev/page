27c521b953d79cee4b7de2d2a2e719e36e21c3e0
Susilloxxxx <susiloabdulmujib@outlook.com>
2025-03-03 16:56:29 +0700

    deploy selasa sore

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./assets/css/custom.css", // Tambahkan path ke file custom.css
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
