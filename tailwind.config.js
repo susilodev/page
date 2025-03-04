fb76c317f9fc30cbdc26caa795db3459b98fc695
Susilloxxxx <susiloabdulmujib@outlook.com>
2025-03-04 09:16:45 +0700

    ready to deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+const typography = require("@tailwindcss/typography");
+
 module.exports = {
   content: [
     "./layouts/**/*.html",
     "./content/**/*.md",
     "./assets/css/custom.css", // Tambahkan path ke file custom.css
+    "./themes/hugoplate/layouts/**/*.html", // Tambahkan path themes
+    "./themes/hugoplate/assets/css/**/*.css", // Tambahkan path assets themes
+    "./assets/css/**/*.css",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
 };
